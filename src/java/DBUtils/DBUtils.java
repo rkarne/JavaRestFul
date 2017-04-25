eff5b05e0cd83f3fdb1cfbd7c641729c993d9cce
Roja Jayashree <c0689497@mylambton.ca>
2017-04-25 04:35:13 -0400

    websocket fixed issues with controller in assignment 7

--- src/java/DBUtils/DBUtils.java
+++ src/java/DBUtils/DBUtils.java
@@ -1,4 +1,4 @@
-package rest;
+package DBUtils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
--- src/java/rest/MessageController.java
+++ src/java/rest/MessageController.java
@@ -1,7 +1,12 @@
 
 package rest;
 
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import DBUtils.DBUtils;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
